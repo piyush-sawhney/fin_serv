57c49340b0e7e72281dfcdb5cda8be8ee7b35da8
Piyush Sawhney <piyushsawhney@outlook.com>
2024-12-21 07:58:45 +0000

    Adding Mutual Fund Folio  Scheme Doctype

---
+++ fin_serv/finserv_investment/doctype/mf_folio_scheme/test_mf_folio_scheme.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, KNAPS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMFFolioScheme(FrappeTestCase):
+	pass
